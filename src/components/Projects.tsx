c5b528caab8f00f49968d6f5c6dec09ab7d991b1
ace-cltr <anand.vish201@gmail.com>
2024-05-15 12:36:34 +0530

    added framer motion and projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import './project.css'
+
+interface Project {
+  name: string;
+  description: string;
+  imageUrl: string;
+  githubUrl: string;
+}
+
+const projects: Project[] = [
+  {
+    name: 'Project 1',
+    description: 'Description of Project 1.',
+    imageUrl: './img/quiz.png',
+    githubUrl: 'https://github.com/ace-cltr/binge-watch',
+  },
+  {
+    name: 'binge Watch',
+    description: 'Responsive and user-friendly movie-data fetching website',
+    imageUrl: './img/Untitled.png',
+    githubUrl: 'https://github.com/ace-cltr/binge-watch',
+  },
+  {
+    name: 'Project 3',
+    description: 'Description of Project 3.',
+    imageUrl: './img/Untitled.png',
+    githubUrl: 'https://github.com/ace-cltr/binge-watch',
+  },
+  {
+    name: 'Project 4',
+    description: 'Description of Project 4.',
+    imageUrl: './img/Untitled.png',
+    githubUrl: 'https://github.com/ace-cltr/binge-watch',
+  },
+];
 
 const Projects: React.FC = () => {
   return (
     <div className="projects">
       <h2>Projects</h2>
-      <div className="project">
-        <h3>Project 1</h3>
-        <a href="http://link-to-project-1.com">
-          <img src="http://link-to-project-1-screenshot.com" alt="Project 1 Screenshot" />
-        </a>
-        <p>Description of Project 1.</p>
+      <div className="projects-grid">
+        {projects.map((project, index) => (
+          <motion.div
+            key={index}
+            className="project"
+            whileHover={{ scale: 1.1, rotate: 2 }}
+            transition={{ type: 'spring', stiffness: 300 }}
+          >
+            <a href={project.githubUrl} target="_blank" rel="no">
+              <img src={project.imageUrl} alt={`${project.name} Screenshot`} />
+            </a>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+          </motion.div>
+        ))}
       </div>
-      <div className="project">
-        <h3>Project 2</h3>
-        <a href="http://link-to-project-2.com">
-          <img src="http://link-to-project-2-screenshot.com" alt="Project 2 Screenshot" />
-        </a>
-        <p>Description of Project 2.</p>
-      </div>
-      {/* Add more projects as needed */}
     </div>
   );
 }
